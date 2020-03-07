e602ba15084d621b4d185b004c7d0f726a782f4c
Danil Usmanov <usmanovdanilr@gmail.com>
2020-03-07 19:47:58 +0300

    I hope it's beta version

--- assignment.pl
+++ assignment.pl
@@ -1,9 +1,12 @@
  % Input: Put here positions of orcs, humans and touchdown as <type>(X,Y)
+%  include(input.pl).  % If you want to include input file with facts, uncomment this line and put file uinput,pl near the current file.
+% Map 3
 o(0, 1).
-o(1,1).
-o(2, 2).
+o(1, 1).
+o(2, 1).
+o(2,2).
+h(3, 0).
 t(0, 2).
-h(3,3).
 
 % ================= Facts =================
 size(4).  % The size of one side of the field
@@ -25,16 +28,17 @@ is_touchdown(X, Y) :-  % Check: is it touchdown point
 
 % ================= SEARCH VARIANTS =================
 choose_search(backtracking, Moves) :- assert(flag(0)), assert(score(0)), backtracking_search(0, 0, Moves, [(0, 0)]).  % Backtracking search
-choose_search(random, Moves) :- assert(flag(0)), assert(score(0)), assert(path([], inf)), retractall(solved(_)), random_loop(100000), retractall(score(_)), path(Moves, Score), assert(score(Score)).  % Random search 
+choose_search(random, Moves) :- assert(flag(0)), assert(score(0)), assert(path([], inf)), retractall(solved(_)), random_loop(1000000), retractall(score(_)), path(Moves, Score), assert(score(Score)).  % Random search 
 choose_search(greedy, Moves) :- assert(flag(0)), assert(score(0)), assert(min_hypt(up, inf)), greedy_search(0, 0, Moves, [(0, 0)]).  % Greedyy search
 
 % ================= BACKTRACKING SEARCH =================
 backtracking_search(X, Y, [], _) :- is_touchdown(X, Y),  % Base case
-    write("Jesus we got it! "), write(X), write(" "), write(Y), nl.
+    write("We got it! "), write(X), write(" "), write(Y), nl.
 backtracking_search(X, Y, [Step | Moves], Visited) :-  % Recursion step
     step(X, Y, X_NEXT, Y_NEXT, Step), 
     check_position(X_NEXT, Y_NEXT, Visited),
-    score(Score), (h(X_NEXT, Y_NEXT) -> true; (retractall(score(_)), Score1 is Score + 1, assert(score(Score1)))),
+    score(Score), (h(X_NEXT, Y_NEXT) -> true; (retractall(score(_)), 
+    Score1 is Score + 1, assert(score(Score1)))),
     backtracking_search(X_NEXT, Y_NEXT, Moves, [(X, Y) | Visited]).
 
 % ================= RANDOM SEARCH =================
@@ -48,7 +52,7 @@ random_loop(NumberAttemp) :-  % Loop step case
     random_loop(NumberAttemp1).
 
 random_search(X, Y, [], _) :- is_touchdown(X, Y), assert(solved(1)),  % Base case
-    write("Jesus we got it! "), write(X), write(" "), write(Y), nl.
+    write("We got it! "), write(X), write(" "), write(Y), nl.
 random_search(X, Y, [Step | Moves], Visited) :-  % Recursion step
     random_member(Step, [up, right, down, left, pass_up, pass_right, pass_down, pass_left]),% pass_up, pass_right, pass_down, pass_left, pass_up_rigth, pass_down_rigth, pass_down_left, pass_up_left]),
     step(X, Y, X_NEXT, Y_NEXT, Step), !, 
@@ -81,7 +85,7 @@ greedy(Step, [First | Tail], X, Y, Visited) :-
     greedy(Step, Tail, X, Y, Visited). 
 
 greedy_search(X, Y, [], _) :- is_touchdown(X, Y),  % Base case
-    write("Jesus we got it! "), write(X), write(" "), write(Y), nl.
+    write("We got it! "), write(X), write(" "), write(Y), nl.
 greedy_search(X, Y, [Step | Moves], Visited) :-  % Recursion step
     retractall(min_hypt(_,_)), assert(min_hypt(up, inf)),
     retractall(flag2(_)), assert(flag2(0)),
@@ -115,7 +119,7 @@ step(X, Y, X_NEXT, Y_NEXT, pass_right) :-  % Pass RIRHT
 step(X, Y, X_NEXT, Y_NEXT, pass_down) :-  % Pass DOWN
     flag(0),
     h(X, Y_NEXT),
-    Y_NEXT > Y,
+    Y_NEXT < Y,
     not((o(X, Y_orc), Y_orc < Y, Y_orc > Y_NEXT)),
     X_NEXT is X,
     retract(flag(_)),
@@ -124,7 +128,7 @@ step(X, Y, X_NEXT, Y_NEXT, pass_down) :-  % Pass DOWN
 step(X, Y, X_NEXT, Y_NEXT, pass_left) :-  % Pass Left
     flag(0),
     h(X_NEXT, Y),
-    X_NEXT > X,
+    X_NEXT < X,
     not((o(X_orc, Y), X_orc < X, X_orc > X_NEXT)),
     Y_NEXT is Y,
     retract(flag(_)),
@@ -181,19 +185,16 @@ step(X, Y, X_NEXT, Y_NEXT, left) :-  % Step Left
 
 % ================= MAIN =================
 go:-
-    % Init
     statistics(runtime,[Start|_]),
 
-
     % Simple backtracking
-    % choose_search(backtracking, Moves),
+    choose_search(backtracking, Moves),
 
     % Random 
     % choose_search(random, Moves),
 
     % Greedy
-    choose_search(greedy, Moves),
-
+    % choose_search(greedy, Moves),
 
     % Calculate output
     statistics(runtime,[Stop|_]),
