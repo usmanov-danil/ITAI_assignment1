cb0ca5884962edefd184f56cd4896296d8b32c9b
Danil Usmanov <usmanovdanilr@gmail.com>
2020-03-08 02:57:20 +0300

    Fix bag in passes, EGOR TNX

--- assignment.pl
+++ assignment.pl
@@ -1,21 +1,14 @@
- % Input: Put here positions of orcs, humans and touchdown as <type>(X,Y)
+% Input: Put here positions of orcs, humans and touchdown as <type>(X,Y)
 %  include(input.pl).  % If you want to include input file with facts, uncomment this line and put file uinput,pl near the current file.
 
-o(2, 1).
-o(2, 4).
-o(2, 7).
-o(5, 6).
-o(5, 8).
-o(7, 1).
-o(8, 5).
-h(5, 2).
-h(6, 5).
-h(3, 5).
-h(1, 8).
-t(9, 9).
+o(0, 1).
+h(3, 2).
+h(2, 3).
+h(2, 2).
+t(3, 3).
 
 % ================= Facts =================
-size(10).  % The size of one side of the field
+size(4).  % The size of one side of the field
 attemp(100000). % The number of attemps for random search
 :- dynamic([flag/1, solved/1, path/2, score/1, min_hypt/2, flag2/1]).  % Dynamic fact
 
@@ -36,16 +29,17 @@ is_touchdown(X, Y) :-  % Check: is it touchdown point
 % ================= SEARCH VARIANTS =================
 choose_search(backtracking, Moves) :- assert(flag(0)), assert(score(0)), backtracking_search(0, 0, Moves, [(0, 0)]).  % Backtracking search
 choose_search(random, Moves) :- assert(flag(0)), assert(score(0)), assert(path([], inf)), retractall(solved(_)), attemp(Attemp), random_loop(Attemp), retractall(score(_)), path(Moves, Score), assert(score(Score)).  % Random search 
-choose_search(greedy, Moves) :- assert(flag(0)), assert(score(0)), assert(min_hypt(up, inf)), greedy_search(0, 0, Moves, [(0, 0)]).  % Greedyy search
+choose_search(greedy, Moves) :- assert(flag(0)), assert(score(0)), assert(min_hypt(up, inf)), greedy_search(0, 0, Moves, [(0, 0)]).  % Greedy search
+choose_search(greedy_improoved, Moves) :- assert(flag(0)), assert(score(0)), assert(min_hypt(up, inf)), greedy_search_imp(0, 0, Moves, [(0, 0)]).  % Improoved greedy search
 
 % ================= BACKTRACKING SEARCH =================
 backtracking_search(X, Y, [], _) :- is_touchdown(X, Y),  % Base case
     write("We got it! "), write(X), write(" "), write(Y), nl.
 backtracking_search(X, Y, [Step | Moves], Visited) :-  % Recursion step
     step(X, Y, X_NEXT, Y_NEXT, Step), 
+    write(X), write(" "), writeln(Y),
     check_position(X_NEXT, Y_NEXT, Visited),
-    score(Score), (h(X_NEXT, Y_NEXT) -> true; (retractall(score(_)), 
-    Score1 is Score + 1, assert(score(Score1)))),
+    score(Score), (h(X_NEXT, Y_NEXT) -> true; (retractall(score(_)), Score1 is Score + 1, assert(score(Score1)))),
     backtracking_search(X_NEXT, Y_NEXT, Moves, [(X, Y) | Visited]).
 
 % ================= RANDOM SEARCH =================
@@ -84,7 +78,6 @@ calc_hypt(X, Y, Step, Length, Visited) :-
     ).
 greedy(Step, [], _, _, _) :- min_hypt(Step, _).
 greedy(Step, [First | Tail], X, Y, Visited) :-
-    % writeln(First),
     calc_hypt(X, Y, First, Length, Visited), min_hypt(_, Min_length),
     (Length < Min_length -> (retractall(min_hypt(_,_)), assert(min_hypt(First, Length))) ; true),
     greedy(Step, Tail, X, Y, Visited). 
@@ -101,6 +94,40 @@ greedy_search(X, Y, [Step | Moves], Visited) :-  % Recursion step
         score(Score), (h(X_NEXT, Y_NEXT) -> true; (retractall(score(_)), 
         Score1 is Score + 1, assert(score(Score1)))),
         greedy_search(X_NEXT, Y_NEXT, Moves, [(X, Y) | Visited])
+    ).
+
+% ================= IMPROOVED GREEDY SEARCH =================
+calc_hypt_imp(X, Y, Step, Length, Visited) :-
+    (flag(0) -> (retractall(flag2(_)), assert(flag2(0))) ; 
+    (retractall(flag2(_)), assert(flag2(1)))),
+    t(X_TOUCH, Y_TOUCH),
+
+    (step(X, Y, X_NEXT, Y_NEXT, Step) -> 
+    (flag2(0) -> (retractall(flag(_)), assert(flag(0))); true),
+    (check_position(X_NEXT, Y_NEXT, Visited) % Add checking that our step in valid area also don't forget to fix passes
+    -> (Length is sqrt((X_TOUCH - X_NEXT)**2 + (Y_TOUCH - Y_NEXT)**2)) ; Length is inf)
+    ;
+        Length is inf
+    ).
+    
+greedy_imp(Step, [], _, _, _) :- min_hypt(Step, _).
+greedy_imp(Step, [First | Tail], X, Y, Visited) :-
+    calc_hypt_imp(X, Y, First, Length, Visited), min_hypt(_, Min_length),
+    (Length < Min_length -> (retractall(min_hypt(_,_)), assert(min_hypt(First, Length))) ; true),
+    greedy(Step, Tail, X, Y, Visited). 
+
+greedy_search_imp(X, Y, [], _) :- is_touchdown(X, Y),  % Base case
+    write("We got it! "), write(X), write(" "), write(Y), nl.
+greedy_search_imp(X, Y, [Step | Moves], Visited) :-  % Recursion step
+    retractall(min_hypt(_,_)), assert(min_hypt(up, inf)),
+    retractall(flag2(_)), assert(flag2(0)),
+    greedy_imp(Step, [up, right, down, left, pass_up, pass_right, pass_down, pass_left, pass_up_rigth, pass_down_rigth, pass_down_left, pass_up_left], X, Y, Visited),
+    step(X, Y, X_NEXT, Y_NEXT, Step), !, 
+    check_position(X_NEXT, Y_NEXT, Visited) -> 
+    (
+        score(Score), (h(X_NEXT, Y_NEXT) -> true; (retractall(score(_)), 
+        Score1 is Score + 1, assert(score(Score1)))),
+        greedy_search_imp(X_NEXT, Y_NEXT, Moves, [(X, Y) | Visited])
     ).
 
 % ================= PASS =================
@@ -146,40 +173,40 @@ step(X, Y, X_NEXT, Y_NEXT, pass_left) :-  % Pass Left
 
 step(X, Y, X_NEXT, Y_NEXT, pass_up_rigth) :-  % Pass UP-Right
     flag(0),
-    h(X_NEXT, Y_NEXT),
-    not((h(X_hum, Y_hum), X_DIF_HUM is X_hum - X, Y_DIF_HUM is Y_hum - Y)),
+    h(X_NEXT, Y_NEXT), (X_NEXT - X) =:= (Y_NEXT - Y),
+    ((h(X_hum, Y_hum), X_DIF_HUM is X_hum - X, Y_DIF_HUM is Y_hum - Y)),
     not(((Y_hum < Y_NEXT), (Y_hum > Y), (X_hum < X_NEXT), (X_hum > X), (X_DIF_HUM is Y_DIF_HUM))),
-    not((o(X_orc, Y_orc), X_DIF is X_orc - X, Y_DIF is Y_orc - Y)),
+    ((o(X_orc, Y_orc), X_DIF is X_orc - X, Y_DIF is Y_orc - Y)),
     not(((Y_orc < Y_NEXT), (Y_orc > Y), (X_orc < X_NEXT), (X_orc > X), (X_DIF is Y_DIF))),
     retract(flag(_)),
     assert(flag(1)).
 
 step(X, Y, X_NEXT, Y_NEXT, pass_down_rigth) :-  % Pass RIRHT-Down
     flag(0),
-    h(X_NEXT, Y_NEXT),
-    not(( h(X_hum, Y_hum), X_DIF_HUM is X_hum - X, Y_DIF_HUM is Y - Y_hum)),
+    h(X_NEXT, Y_NEXT), (X_NEXT - X) =:= (Y - Y_NEXT),
+    (( h(X_hum, Y_hum), X_DIF_HUM is X_hum - X, Y_DIF_HUM is Y - Y_hum)),
     not(((Y_hum > Y_NEXT), (Y_hum < Y), (X_hum < X_NEXT), (X_hum > X), (X_DIF_HUM is Y_DIF_HUM))),
-    not(( o(X_orc, Y_orc), X_DIF is X_orc - X, Y_DIF is Y - Y_orc)),
+    (( o(X_orc, Y_orc), X_DIF is X_orc - X, Y_DIF is Y - Y_orc)),
     not(((Y_orc > Y_NEXT), (Y_orc < Y), (X_orc < X_NEXT), (X_orc > X), (X_DIF is Y_DIF))),
     retract(flag(_)),
     assert(flag(1)).
 
 step(X, Y, X_NEXT, Y_NEXT, pass_down_left) :-  % Pass Left-DOWN
     flag(0),
-    h(X_NEXT, Y_NEXT),
-    not((h(X_hum, Y_hum), X_DIF_HUM is X - X_hum, Y_DIF_HUM is Y - Y_hum)),
+    h(X_NEXT, Y_NEXT), (X - X_NEXT) =:= (Y - Y_NEXT),
+    ((h(X_hum, Y_hum), X_DIF_HUM is X - X_hum, Y_DIF_HUM is Y - Y_hum)),
     not(((Y_hum > Y_NEXT), (Y_hum < Y), (X_hum > X_NEXT), (X_hum < X), (X_DIF_HUM is Y_DIF_HUM))),
-    not((o(X_orc, Y_orc), X_DIF is X - X_orc, Y_DIF is Y - Y_orc)),
+    ((o(X_orc, Y_orc), X_DIF is X - X_orc, Y_DIF is Y - Y_orc)),
     not(((Y_orc > Y_NEXT), (Y_orc < Y), (X_orc > X_NEXT), (X_orc < X), (X_DIF is Y_DIF))),
     retract(flag(_)),
     assert(flag(1)).
 
 step(X, Y, X_NEXT, Y_NEXT, pass_up_left) :-  % Pass Up-Left
     flag(0),
-    h(X_NEXT, Y_NEXT),
-    not((h(X_hum, Y_hum), X_DIF_HUM is X - X_hum, Y_DIF_HUM is Y_hum - Y)),
+    h(X_NEXT, Y_NEXT), (X - X_NEXT) =:= (Y_NEXT - Y),
+    ((h(X_hum, Y_hum), X_DIF_HUM is X - X_hum, Y_DIF_HUM is Y_hum - Y)),
     not(((Y_hum < Y_NEXT), (Y_hum > Y), (X_hum > X_NEXT), (X_hum < X), (X_DIF_HUM is Y_DIF_HUM))),
-    not((o(X_orc, Y_orc), X_DIF is X - X_orc, Y_DIF is Y_orc - Y)),
+    ((o(X_orc, Y_orc), X_DIF is X - X_orc, Y_DIF is Y_orc - Y)),
     not(((Y_orc < Y_NEXT), (Y_orc > Y), (X_orc > X_NEXT), (X_orc < X), (X_DIF is Y_DIF))),
     retract(flag(_)),
     assert(flag(1)).
@@ -206,13 +233,16 @@ go:-
     statistics(runtime,[Start|_]),
 
     % Simple backtracking
-    % choose_search(backtracking, Moves),
+    choose_search(backtracking, Moves),
 
     % Random 
     % choose_search(random, Moves),
 
     % Greedy
-    choose_search(greedy, Moves),
+    % choose_search(greedy, Moves),
+
+    % Improoved greedy search
+    % choose_search(greedy_improoved, Moves),
 
     % Calculate output
     statistics(runtime,[Stop|_]),
